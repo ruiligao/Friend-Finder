878eabfe57b10766bd04e63b213ac932e5f5ff40
ruiligao <ruiligao@gmail.com>
2019-04-05 22:14:02 -0700

    rg

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require ("express");
 var app = express();
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 7000;
 var  path = require("path");
 
 app.use(express.urlencoded({ extended: true}));
